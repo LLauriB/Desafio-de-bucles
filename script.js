df2d1ec30eb38eff9d1479826830189058e2392a
LLauriB <166570088+LLauriB@users.noreply.github.com>
2024-06-03 00:22:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+for (var i=1; i<21; i+=2){
+        console.log(i);
+}
+
+
+for(var i=100; i>0; i--){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+
+var h=4;
+while(h>-4){
+    console.log(h)
+    h-=1.5;
+}
+ 
+var j=0;
+var sum=0
+while(j<=100){
+    sum+=j;
+    j++;
+}
+console.log(sum)
+
+var product=1
+for(var i=1; i<=12; i++){
+    product*=i;
+}
+console.log(product)
